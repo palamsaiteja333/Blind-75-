c3b85350f773c68b66c262b6e955f4664988f282
Sai Teja <51771508+palamsaiteja333@users.noreply.github.com>
2025-05-07 19:32:38 -0300

    [49] [Time Beats: 5.05%] [Memory Beats: 6.11%] - LeetPush

--- Group-Anagrams.java
+++ Group-Anagrams.java
@@ -1,18 +1,22 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
-Map<String, List<String>> map = new HashMap<>();
-        for(String s : strs){
+        Map<String, List<String>> result = new HashMap<>();
+
+        for(String s: strs){
             int[] count = new int[26];
             for(char c: s.toCharArray()){
                 count[c - 'a']++;
             }
 
             String key = Arrays.toString(count);
-            map.putIfAbsent(key, new ArrayList<>());
-            map.get(key).add(s);
+
+            result.putIfAbsent(key, new ArrayList<>());
+
+            result.get(key).add(s);
         }
+
+        return new ArrayList<>(result.values());
         
-        return new ArrayList<>(map.values());
     }
 }
