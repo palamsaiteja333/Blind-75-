1a0379e5c82b03e461ff5ab9c2d91e646cc52d25
Sai Teja <51771508+palamsaiteja333@users.noreply.github.com>
2025-05-07 18:55:15 -0300

    [242] [Time Beats: 55.35%] [Memory Beats: 68.62%] - LeetPush

--- Valid-Anagram.java
+++ Valid-Anagram.java
@@ -4,21 +4,21 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        
 
         int[] count = new int[26];
-
         for(int i=0; i<s.length(); i++){
             count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+            count[t.charAt(i)-'a']--;
         }
-
-        for(int indexValue : count){
-            if(indexValue > 0){
+        
+        for(int value: count){
+            if(value > 0){
                 return false;
             }
         }
 
         return true;
     }
+
+
 }
