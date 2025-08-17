4c2b43f8efc6ed4783348b3a5c5f6082ece83bdd
Sai Teja <51771508+palamsaiteja333@users.noreply.github.com>
2025-08-17 10:06:47 -0300

    [217] [Time Beats: 46.84%] [Memory Beats: 88.71%] - LeetPush

--- Contains-Duplicate.java
+++ Contains-Duplicate.java
@@ -3,17 +3,15 @@ class Solution {
 
         Set<Integer> set = new HashSet<>();
 
-        for(int i=0; i<nums.length; i++){
-            if(set.contains(nums[i])){
+        for(int num: nums){
+            if(set.contains(num)){
                 return true;
-            }else{
-                set.add(nums[i]);
             }
+
+            set.add(num);
         }
 
         return false;
-
-
         
     }
 }
