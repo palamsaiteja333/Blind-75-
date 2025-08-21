12969ac43c303a5aede12436962d56db0c6b0c5d
Sai Teja <51771508+palamsaiteja333@users.noreply.github.com>
2025-08-21 15:12:34 -0300

    [242] [Time Beats: 42.36%] [Memory Beats: 97.85%] - LeetPush

--- Valid-Anagram.java
+++ Valid-Anagram.java
@@ -7,15 +7,15 @@ class Solution {
 
         int[] charArray = new int[26];
 
-        for(int i=0; i<s.length();i++){
+        for(int i=0; i<s.length(); i++){
+
             charArray[s.charAt(i) - 'a']++;
-            charArray[t.charAt(i) - 'a']--;
-            
+            charArray[t.charAt(i)  - 'a']--;
         }
 
         for(int n: charArray){
             if(n>0){
-return false;
+                return false;
             }
         }
 
