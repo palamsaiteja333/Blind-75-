20fba37acef54f32ccf8faea21cb2a77b633f9d6
Sai Teja <51771508+palamsaiteja333@users.noreply.github.com>
2025-08-23 16:21:31 -0300

    [1] [Time Beats: 98.89%] [Memory Beats: 30.62%] - LeetPush

--- Two-Sum.java
+++ Two-Sum.java
@@ -5,14 +5,15 @@ class Solution {
 
         for(int i=0; i<nums.length; i++){
             int diff = target - nums[i];
+
             if(map.containsKey(diff)){
                 return new int[]{i, map.get(diff)};
             }
 
             map.put(nums[i], i);
         }
-        
 
         return new int[]{};
+        
     }
 }
