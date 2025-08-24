e500e27ee9d69f8457bf4ee5f1b1b062de4152db
Sai Teja <51771508+palamsaiteja333@users.noreply.github.com>
2025-08-24 15:41:28 -0300

    [128] [Time Beats: 38.37%] [Memory Beats: 22.29%] - LeetPush

--- Longest-Consecutive-Sequence.java
+++ Longest-Consecutive-Sequence.java
@@ -1,33 +1,3 @@
-// class Solution {
-//     public int longestConsecutive(int[] nums) {
-
-//         Set<Integer> set = new HashSet<>();
-
-//         for(int n: nums){
-//             set.add(n);
-//         }
-
-//         int result =0;
-
-//         for(int n: nums){
-//             if(!set.contains(n-1)){
-//                 int length=1;
-
-//                 while(set.contains(n + length)){
-//                     length++;
-//                 }
-
-//                 result = Math.max(result, length);
-//             }
-//         }
-
-//         return result;
-        
-//     }
-// }
-
-
-
 public class Solution {
     public int longestConsecutive(int[] nums) {
         Set<Integer> numSet = new HashSet<>();
